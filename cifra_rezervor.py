41af60cea953e024f50429ff2ed388bdc407ca59
Radu Mirescu <radumirescu4@gmail.com>
2017-11-25 18:05:31 +0200

    schitat solutie

--- cifra_rezervor.py
+++ cifra_rezervor.py
@@ -5,6 +5,12 @@ class CifraRezervor():
             for coloana in [1, 2, 3]:
                 self.patrate.append(Patrat(rand, coloana))
 
+    def deseneaza(self, x, y, tema):
+        result = []
+        for patrat in self.patrate:
+            result += patrat.deseneaza(x, y, tema)
+        return result
+
 
 class Patrat():
     def __init__(self, rand, coloana):
@@ -16,3 +22,28 @@ class Patrat():
 
     def y_absolut(self):
         return self.rand - 1
+
+    def deseneaza(self, x, y, tema):
+        result = []
+        result += [self.coloreaza_patrat(x, y, tema)]
+        result += [self.bordura_sus(x, y, tema)]
+        result += [self.bordura_dreapta(x, y, tema)]
+        result += [self.bordura_jos(x, y, tema)]
+        result += [self.bordura_stanga(x, y, tema)]
+        return result
+
+    def coloreaza_patrat(self, x, y, tema):
+        pass
+
+    def bordura_sus(self, x, y, tema):
+        # return ['line', x1, y1, x2, y2, color]
+        pass
+
+    def bordura_dreapta(self, x, y, tema):
+        pass
+
+    def bordura_jos(self, x, y, tema):
+        pass
+
+    def bordura_stanga(self, x, y, tema):
+        pass
